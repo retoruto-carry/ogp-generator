9796d059012ffb44e094046bf86e4968bc786df7
retoruto-carry <k.yuta.happy.123456789@gmail.com>
2019-07-21 16:31:08 +0900

    refactor: 変換済みの白いボックス画像を用意するようにした

--- index.js
+++ index.js
@@ -2,6 +2,19 @@ require('dotenv').config();
 var cloudinary = require('cloudinary').v2;
 var fs = require('fs');
 
+const commandLineArgs = require('command-line-args');
+const optionDefinitions = [
+  {
+    name: 'image',
+    type: String
+  },
+  {
+    name: 'title',
+    type: String,
+  },
+];
+const options = commandLineArgs(optionDefinitions);
+
 // set your env variable CLOUDINARY_URL or set the following configuration
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
@@ -9,11 +22,11 @@ cloudinary.config({
   api_secret: process.env.API_SECRET
 });
 
-cloudinary.uploader.upload('https://res.cloudinary.com/dec7on5qy/image/upload/v1563630264/hamburg.jpg',
+cloudinary.uploader.upload(options.image,
     {transformation: [
         {gravity: "auto", height: 1080, width: 1920, crop: "thumb"},
-        {height: 800, overlay: "white_r1kryq", opacity: 60, radius: 30, width: 1700, crop: "scale"},
-        {width: 1700, overlay: {font_family: "Sawarabi Gothic", font_size: 130, font_weight: "bold", text: "Twitterで話題の%0A「ハンバーガーの日」%0Aについて調べてみた"}, color: "#333", crop: "fit"}
+        {overlay: "white_box"},
+        {width: 1700, overlay: {font_family: "Sawarabi Gothic", font_size: 130, font_weight: "bold", text: options.title}, color: "#333", crop: "fit"}
     ]},
     function (err, image) {
         if (err) { console.warn(err); }
