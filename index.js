e3054e204f78dbec21e2bbe8581907382a14d1dd
retoruto-carry <k.yuta.happy.123456789@gmail.com>
2019-07-21 15:26:57 +0900

    add: 文字をオーバーレイするようにした

--- index.js
+++ index.js
@@ -14,7 +14,8 @@ console.log("** ** ** ** ** ** ** ** ** Uploads ** ** ** ** ** ** ** ** ** **");
 cloudinary.uploader.upload('https://res.cloudinary.com/dec7on5qy/image/upload/v1563630264/hamburg.jpg',
     {transformation: [
         {gravity: "auto", height: 1080, width: 1920, crop: "thumb"},
-        {height: 800, overlay: "white_r1kryq", opacity: 60, radius: 30, width: 1700, crop: "scale"}
+        {height: 800, overlay: "white_r1kryq", opacity: 60, radius: 30, width: 1700, crop: "scale"},
+        {width: 1700, overlay: {font_family: "Sawarabi Gothic", font_size: 130, font_weight: "bold", text: "Twitterで話題の%0A「ハンバーガーの日」%0Aについて調べてみた"}, color: "#333", crop: "fit"}
     ]},
     function (err, image) {
         console.log();
